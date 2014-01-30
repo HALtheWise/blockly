64def3451dbe9c95697adc6ea3599f5e55312a7c
Eric Miller <eric@legoaces.org>
2014-01-30 17:31:32 -0500

    Made file for generated blocks

---
+++ apps/ftc/generatedblocks.js
@@ -0,0 +1,8 @@
+/**
+ * This file contains code to generate blocks that consist of simple API calls.
+ * It also has a parseC function that takes a line of C code like:
+ * 'void noReturnFunction(){ //This function does stuff'
+ * 		and returns a simpleblock format object.
+ * 
+ * @author eric@legoaces.org (Eric Miller)
+ */
